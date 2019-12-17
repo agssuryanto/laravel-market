db3f7f24f0bdc533ba4c627a8dc02db6d788cfcf
Agus Suryanto <ags.suryanto@gmail.com>
2019-12-17 21:13:26 +0700

    Update frontend 1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -44,8 +44,10 @@
                         </div>
                     </div>
                 </ul>
-                <button class="btn btn-outline-secondary" type="button" id="button-addon2" style="font-size: 24px; border: none !important;"><ion-icon name="cart"></ion-icon></button>
+                
                 <form class="form-inline my-2 my-lg-0">
+                    <button class="btn btn-outline-secondary mr-3" type="button" id="button-addon2" style="font-size: 24px; border: none !important;"><ion-icon name="cart"></ion-icon></button>
+                    <span  style="font-size: 24px; font-weight: 100;">|<span>
                     <button class="btn btn-outline-success mx-2 my-2 my-sm-0" type="submit">Masuk</button>
                     <button class="btn btn-success mx-2 my-2 my-sm-0" type="submit">Daftar</button>
                 </form>                
@@ -59,6 +61,7 @@
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-    <script src="https://unpkg.com/ionicons@4.5.10-0/dist/ionicons.js"></script>
+    <script type="module" src="https://unpkg.com/ionicons@4.5.10-0/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule="" src="https://unpkg.com/ionicons@4.5.10-0/dist/ionicons/ionicons.js"></script>
   </body>
 </html>
