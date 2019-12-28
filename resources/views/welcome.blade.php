7fb262430e7a18b6934c07dbb62906caacf62c57
Agus Suryanto <ags.suryanto@gmail.com>
2019-12-28 13:28:33 +0700

    update frontend

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -73,12 +73,12 @@
 
     <div class="container mt-5 pt-5">
         <div class="slider owl-carousel owl-theme">
-            <div><img src="{{ asset('/images/slider/slide1.jpg') }}"></div>
-            <div><img src="{{ asset('/images/slider/slide2.jpg') }}"></div>
-            <div><img src="{{ asset('/images/slider/slide3.jpg') }}"></div>
-            <div><img src="{{ asset('/images/slider/slide4.jpg') }}"></div>
-            <div><img src="{{ asset('/images/slider/slide5.jpg') }}"></div>
-            <div><img src="{{ asset('/images/slider/slide6.jpg') }}"></div>
+            <div><img src="{{ config('app.url') }}/storage/slider/slide1.jpg"></div>
+            <div><img src="{{ config('app.url') }}/storage/slider/slide2.jpg"></div>
+            <div><img src="{{ config('app.url') }}/storage/slider/slide3.jpg"></div>
+            <div><img src="{{ config('app.url') }}/storage/slider/slide4.jpg"></div>
+            <div><img src="{{ config('app.url') }}/storage/slider/slide5.jpg"></div>
+            <div><img src="{{ config('app.url') }}/storage/slider/slide6.jpg"></div>
         </div>
     </div>
 
@@ -89,42 +89,42 @@
                 <div class="category owl-carousel owl-theme">
                     <div>
                         <div class="category-carousel-box pt-2">
-                            <img style="width: 48px;" src="{{ asset('/images/product_category/accesories_icon.jpeg') }}">
+                            <img style="width: 48px;" src="{{ config('app.url') }}/storage/product_category/accesories_icon.jpeg">
                             <div class="clearfix">&nbsp;</div>
                             <h6>Accesories</h6>
                         </div>
                     </div>
                     <div>
                         <div class="category-carousel-box pt-2">
-                            <img style="width: 48px;" src="{{ asset('/images/product_category/craft_icon.png') }}">
+                            <img style="width: 48px;" src="{{ config('app.url') }}/storage/product_category/craft_icon.png">
                             <div class="clearfix">&nbsp;</div>
                             <h6>Craft</h6>
                         </div>
                     </div>                    
                     <div>
                         <div class="category-carousel-box pt-2">
-                            <img style="width: 48px;" src="{{ asset('/images/product_category/drink_icon.jpg') }}">
+                            <img style="width: 48px;" src="{{ config('app.url') }}/storage/product_category/drink_icon.jpg">
                             <div class="clearfix">&nbsp;</div>
                             <h6>Drink</h6>                       
                         </div>
                     </div>                                        
                     <div>
                         <div class="category-carousel-box pt-2">
-                            <img style="width: 48px;" src="{{ asset('/images/product_category/fashion_icon.jpg') }}">
+                            <img style="width: 48px;" src="{{ config('app.url') }}/storage/product_category/fashion_icon.jpg">
                             <div class="clearfix">&nbsp;</div>
                             <h6>Fashion</h6>                      
                         </div>
                     </div>                                                            
                     <div>
                         <div class="category-carousel-box pt-2">
-                            <img style="width: 48px;" src="{{ asset('/images/product_category/food_icon.png') }}">
+                            <img style="width: 48px;" src="{{ config('app.url') }}/storage/product_category/food_icon.png">
                             <div class="clearfix">&nbsp;</div>
                             <h6>Food</h6>                     
                         </div>
                     </div>                                                                                
                     <div>
                         <div class="category-carousel-box pt-2">
-                            <img style="width: 48px;" src="{{ asset('/images/product_category/furniture_icon.png') }}">
+                            <img style="width: 48px;" src="{{ config('app.url') }}/storage/product_category/furniture_icon.png">
                             <div class="clearfix">&nbsp;</div>
                             <h6>Furniture</h6>                     
                         </div>
@@ -226,44 +226,71 @@
         </div>
     </div>
 
-    <!-- PRODUCT TERBARU -->
-    {{-- <div class="container">
-        <div class="row">
-            <div class="col-12">
-                <h6>Product Terbaru</h6>
-            </div>
-        </div>
-    </div> --}}
-    <!-- PRODUCT TERBARU -->
-
-    <!-- PRODUCT TERLARIS -->
-    {{-- <div class="container">
-        <div class="row">
-            <div class="col-12">
-                <h6>Product Terlaris</h6>
-            </div>
-        </div>
-    </div> --}}
-    <!-- PRODUCT TERLARIS -->
-
     <!-- PRODUCT -->
-    <div class="container">
-        <ul class="nav nav-pills nav-fill">
+    <!-- <div class="container">
+        <ul class="nav nav-pills nav-fill" id="pills-tab" role="tablist">
             <li id="product" class="nav-item mx-1" style="background: linear-gradient(90deg, rgba(152,5,5,1) 30%, rgba(245,71,71,1) 100%, rgba(196,20,142,1) 100%); border-radius: 5px;">
-                <a class="nav-link active" href="#">Product Terbaru</a>
+                <a class="nav-link active" data-toggle="pill" href="pills-terbaru">Product Terbaru</a>
             </li>
             <li id="product" class="nav-item mx-1" style="background: linear-gradient(90deg, rgba(152,128,5,1) 30%, rgba(245,226,71,1) 100%, rgba(196,20,142,1) 100%); border-radius: 5px;">
-                <a class="nav-link" href="#">Product Terlaris</a>
+                <a class="nav-link" data-toggle="pill" href="pills-terlaris">Product Terlaris</a>
             </li>
             <li id="product" class="nav-item mx-1" style="background: linear-gradient(90deg, rgba(5,152,78,1) 30%, rgba(20,196,41,1) 100%, rgba(71,245,83,1) 100%); border-radius: 5px;">
-                <a class="nav-link" href="#">Product Discount</a>
+                <a class="nav-link" data-toggle="pill" href="pills-discount">Product Discount</a>
             </li>
             <li id="product" class="nav-item mx-1" style="background: linear-gradient(90deg, rgba(39,5,152,1) 30%, rgba(71,203,245,1) 100%, rgba(196,20,142,1) 100%); border-radius: 5px;">
-                <a class="nav-link" href="#">Product MurMer</a>
+                <a class="nav-link" data-toggle="pill" href="pills-murmer">Product MurMer</a>
             </li>            
         </ul>
+        <div class="tab-content" id="pills-tabContent">
+            <div class="tab-pane fade show active" id="pills-terbaru" role="tabpanel" aria-labelledby="pills-home-tab">
+                <h6>Terbaru</h6>                
+            </div>
+            <div class="tab-pane fade" id="pills-terlaris" role="tabpanel" aria-labelledby="pills-profile-tab">
+                <h6>Terlaris</h6>
+            </div>
+            <div class="tab-pane fade" id="pills-discount" role="tabpanel" aria-labelledby="pills-contact-tab">
+                <h6>Discount</h6>
+            </div>
+            <div class="tab-pane fade" id="pills-murmer" role="tabpanel" aria-labelledby="pills-contact-tab">
+                <h6>Murah Meriah</h6>
+            </div>
+        </div>        
+    </div> -->
+
+    <div class="container">
+        <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
+            <li id="product" class="nav-item mx-1 my-1" style="background: linear-gradient(90deg, rgba(152,5,5,1) 30%, rgba(245,71,71,1) 100%, rgba(196,20,142,1) 100%); border-radius: 5px;">
+                <a class="nav-link active" id="pills-home-tab" data-toggle="pill" href="#pills-home" role="tab" aria-controls="pills-home" aria-selected="true">Product Terbaru</a>
+            </li>
+            <li id="product" class="nav-item mx-1 my-1" style="background: linear-gradient(90deg, rgba(152,128,5,1) 30%, rgba(245,226,71,1) 100%, rgba(196,20,142,1) 100%); border-radius: 5px;">
+                <a class="nav-link" id="pills-profile-tab" data-toggle="pill" href="#pills-profile" role="tab" aria-controls="pills-profile" aria-selected="false">Product Terlaris</a>
+            </li>
+            <li id="product" class="nav-item mx-1 my-1" style="background: linear-gradient(90deg, rgba(5,152,78,1) 30%, rgba(20,196,41,1) 100%, rgba(71,245,83,1) 100%); border-radius: 5px;">
+                <a class="nav-link" id="pills-contact-tab" data-toggle="pill" href="#pills-contact" role="tab" aria-controls="pills-contact" aria-selected="false">Product Discount</a>
+            </li>
+            <li id="product" class="nav-item mx-1 my-1" style="background: linear-gradient(90deg, rgba(39,5,152,1) 30%, rgba(71,203,245,1) 100%, rgba(196,20,142,1) 100%); border-radius: 5px;">
+                <a class="nav-link" id="pills-murmer-tab" data-toggle="pill" href="#pills-murmer" role="tab" aria-controls="pills-contact" aria-selected="false">Product Murmer</a>
+            </li>            
+        </ul>
+        <div class="tab-content" id="pills-tabContent">
+            <div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab">
+                <h6>Satu</h6>
+            </div>
+            <div class="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab">
+                <h6>Dua</h6>
+            </div>
+            <div class="tab-pane fade" id="pills-contact" role="tabpanel" aria-labelledby="pills-contact-tab">
+                <h6>Tiga</h6>
+            </div>
+            <div class="tab-pane fade" id="pills-murmer" role="tabpanel" aria-labelledby="pills-murmer-tab">
+                <h6>Empat</h6>
+            </div>            
+        </div>
     </div>
+
     <!-- PRODUCT -->
+
 
     <!-- FOOTER -->
     <div class="container">
